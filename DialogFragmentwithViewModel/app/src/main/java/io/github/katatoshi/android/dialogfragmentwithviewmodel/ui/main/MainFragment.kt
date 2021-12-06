352f1cb914cf5cfdba324a2a11263a0a37490787
Toshifumi Katayama <kata.toshi@gmail.com>
2021-12-07 01:56:00 +0900

    Update to use screen specific DialogFragment
    Add ResetCounterDialogFragment class in MainFragment class.
    Add VM (ViewModel) class in MainFragment.ResetCounterDialogFragment class.
    Add ResetCounterDialogFragment class in SubFragment class.
    Add VM (ViewModel) class in SubFragment.ResetCounterDialogFragment class.
    Deprecate AlertDialogFragment and AlertDialogViewModel.

--- DialogFragmentwithViewModel/app/src/main/java/io/github/katatoshi/android/dialogfragmentwithviewmodel/ui/main/MainFragment.kt
+++ DialogFragmentwithViewModel/app/src/main/java/io/github/katatoshi/android/dialogfragmentwithviewmodel/ui/main/MainFragment.kt
@@ -1,18 +1,22 @@
 package io.github.katatoshi.android.dialogfragmentwithviewmodel.ui.main
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import androidx.navigation.fragment.findNavController
 import io.github.katatoshi.android.dialogfragmentwithviewmodel.R
 import io.github.katatoshi.android.dialogfragmentwithviewmodel.databinding.FragmentMainBinding
-import io.github.katatoshi.android.dialogfragmentwithviewmodel.ui.dialog.AlertDialogFragment
-import io.github.katatoshi.android.dialogfragmentwithviewmodel.ui.dialog.AlertDialogViewModel
 
 class MainFragment : Fragment() {
 
@@ -30,8 +34,7 @@ class MainFragment : Fragment() {
 
         viewModel.showResetCounterAlert.observe(viewLifecycleOwner) {
             if (it) {
-                AlertDialogFragment.newInstance("Reset counter? (MainFragment)", "Yes", "No")
-                    .show(parentFragmentManager, "reset_counter_alert")
+                ResetCounterDialogFragment().show(parentFragmentManager, "reset_counter_alert")
                 viewModel.doneShowingResetCounterAlert()
             }
         }
@@ -43,21 +46,44 @@ class MainFragment : Fragment() {
             }
         }
 
-        val alertDialogViewModel by activityViewModels<AlertDialogViewModel>()
+        val resetCounterDialogFragmentVM by activityViewModels<ResetCounterDialogFragment.VM>()
 
-        alertDialogViewModel.positive.observe(viewLifecycleOwner) {
+        resetCounterDialogFragmentVM.positive.observe(viewLifecycleOwner) {
             if (it) {
                 viewModel.resetCounter()
-                alertDialogViewModel.donePositive()
-            }
-        }
-
-        alertDialogViewModel.negative.observe(viewLifecycleOwner) {
-            if (it) {
-                alertDialogViewModel.doneNegative()
+                resetCounterDialogFragmentVM.donePositive()
             }
         }
 
         return binding.root
+    }
+
+    class ResetCounterDialogFragment : DialogFragment() {
+
+        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+            val viewModel by activityViewModels<VM>()
+            val builder = AlertDialog.Builder(requireActivity())
+            builder
+                .setMessage("Reset counter? (MainFragment)")
+                .setPositiveButton("Yes") { _, _ -> viewModel.onPositive() }
+                .setNegativeButton("No") { _, _ -> }
+            return builder.create()
+        }
+
+        class VM : ViewModel() {
+
+            private val _positive: MutableLiveData<Boolean> = MutableLiveData(false)
+
+            val positive: LiveData<Boolean>
+                get() = _positive
+
+            fun onPositive() {
+                _positive.value = true
+            }
+
+            fun donePositive() {
+                _positive.value = false
+            }
+        }
     }
 }
